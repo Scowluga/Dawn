9dff10e032dd1c7f8239f0dd2eca4cf03c3aef81
David Lu <david.scowluga@gmail.com>
2019-01-13 22:25:03 -0500

    wrapping up

--- app/src/main/java/com/scowluga/android/dawn/MainActivity.kt
+++ app/src/main/java/com/scowluga/android/dawn/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.scowluga.android.dawn
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.view.WindowManager
-
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
+import me.rishabhkhanna.customtogglebutton.CustomToggleButton
+
 
 class MainActivity : AppCompatActivity() {
+
+    fun displayError(a: Activity) {
+        Toast.makeText(a, "Please Connect to Dawn Device", Toast.LENGTH_SHORT).show()
+    }
+
+    lateinit var refreshBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +32,45 @@ class MainActivity : AppCompatActivity() {
         // set status bar transparent for parallax toolbar
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
 
+        // Refresh Next Alarm
+        val nextTV: TextView = findViewById(R.id.nextTV)
+        refreshBtn = findViewById(R.id.refreshDisplay)
+        refreshBtn.setOnClickListener(View.OnClickListener {
+            val nextAlarm = Settings.System.getString(getContentResolver(),
+                    Settings.System.NEXT_ALARM_FORMATTED)
+            if (nextAlarm.isEmpty())
+                nextTV.setText("Next Alarm: N/A")
+            else
+                nextTV.setText("Next Alarm: " + nextAlarm)
+        })
+        refreshBtn.performClick()
 
+        // Toggle Button
+        val toggle: CustomToggleButton = findViewById(R.id.toggleButton)
+        toggle.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            refreshBtn.performClick()
+            if (isChecked) { // ON
+                val layout: LinearLayout = findViewById(R.id.alarm_ll)
+                for (i in 0 until layout.childCount) {
+                    val child = layout.getChildAt(i)
+                    child.isEnabled = true
+                }
+                displayError(this)
+            } else { // OFF
+                val layout: LinearLayout = findViewById(R.id.alarm_ll)
+                for (i in 0 until layout.childCount) {
+                    val child = layout.getChildAt(i)
+                    child.isEnabled = false
+                }
+                Toast.makeText(this, "Blind Opening Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        })
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        refreshBtn.performClick()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
